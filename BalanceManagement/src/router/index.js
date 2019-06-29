b0842a37f9d6ad75924549fd5a96a7b193bb52dc
viniethss <viniethss@gmail.com>
2019-06-30 03:57:22 +0530

    table in vue changes

--- BalanceManagement/src/router/index.js
+++ BalanceManagement/src/router/index.js
@@ -1,16 +1,28 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import Router from 'vue-router'
 import BalanceManagement from '@/components/BalanceManagement'
+import HomePage from '@/components/HomePage'
+import 'vuetify/dist/vuetify.min.css'
+
 
 Vue.use(Router)
+Vue.use(Vuetify)
 
 export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/balancemanagement',
-      name: 'BalanceManagement',
+      path: '/bm',
       component: BalanceManagement
+    },
+    {
+      path: '/',
+      component: HomePage
+    },
+    {
+      path: '*', 
+      redirect: '/'
     }
   ]
 })
